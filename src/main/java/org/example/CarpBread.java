466031b3e02512e86362db98282f09db11db1eab
kw <kw@n.c>
2024-08-01 21:43:02 +0900

    add static method in CarpBread class

--- src/main/java/org/example/CarpBread.java
+++ src/main/java/org/example/CarpBread.java
@@ -15,4 +15,11 @@ public class CarpBread {
         System.out.println("<<< 조리 시작 >>>");
         System.out.println("[정보] 색상 : " + color + " 이름 : " + name);
     }
+
+    public static boolean validateColor(String color) {
+        if (color.equals("mint") || color.equals("brown") || color.equals("yellow") || color.equals("gray")) {
+            return true;
+        }
+        throw new IllegalArgumentException("유효하지 않은 색상입니다: " + color);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        CarpBread mintCarpBread = new CarpBread(1, "mint", "민트 붕어빵");
-        CarpBread burntCarpBread = new CarpBread(2, "brown", "탄 붕어빵");
-        CarpBread redBeanCarpBread = new CarpBread(3, "yellow", "팥 붕어빵");
-        CarpBread creamCarpBread1 = new CarpBread(4, "gray", "크림 붕어빵");
-        CarpBread creamCarpBread2 = new CarpBread(5, "gray", "크림 붕어빵");
+        CarpBread mintCarpBread = null;
+        CarpBread orangeCarpBread = null;
+        if (CarpBread.validateColor("mint")) {
+            mintCarpBread = new CarpBread(1, "mint", "민트 붕어빵");
+        }
+        if (CarpBread.validateColor("orange")) {
+            orangeCarpBread = new CarpBread(5, "orange", "오랜지 붕어빵");
+        }
 
         mintCarpBread.cook();
-        burntCarpBread.cook();
-        redBeanCarpBread.cook();
-        creamCarpBread1.cook();
-        creamCarpBread2.cook();
+        orangeCarpBread.cook();
     }
 }
-
