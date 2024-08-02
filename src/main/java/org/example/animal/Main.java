eda842d125b7fe4fdd7ea904a8d2a415d192aa35
kw <kw@n.c>
2024-08-02 15:08:03 +0900

    create animal interface and implement example

---
+++ src/main/java/org/example/animal/Main.java
@@ -0,0 +1,15 @@
+package org.example.animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.makeSound();
+        dog.move();
+
+        cat.makeSound();
+        cat.move();
+    }
+}
+
