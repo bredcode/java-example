de04ac9095e6b3bfa04a21a5a7701a6fd42469ba
kw <kw@n.c>
2024-08-11 21:45:23 +0900

    create recursive examples

---
+++ src/main/java/org/example/recursive/parentheses/Main.java
@@ -0,0 +1,9 @@
+package org.example.recursive.parentheses;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String ret = solution.solution(")()()(");
+        System.out.println(ret);
+    }
+}
